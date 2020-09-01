226f122b2d1b9bfe2da7a295eb9ef8c0a8fa221b
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-09-01 17:31:17 +0500

    designing UI from instagram

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,24 @@
         <div class="col-9 p-5">
                 <div><h1>Free Code Camp</h1></div>
                 <div class="d-flex">
-                    <div class="pr-5"><strong>153</strong>Post</div>
-                    <div class="pr-5"><strong>23k</strong>Followers</div>
-                    <div class="pr-5"><strong>212</strong>Folowing</div>
+                    <div class="pr-5"><strong class="font-weight-bold">153</strong>Post</div>
+                    <div class="pr-5"><strong class="font-weight-bold">23k</strong>Followers</div>
+                    <div class="pr-5"><strong class="font-weight-bold">212</strong>Folowing</div>
                 </div>
+                <div class="pt-4">freecodecamp.org</div>
+                <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+                <div><a href="#" class="font-weight-bold">freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fkhi10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.112.929.929a/s640x640/118605679_1451236268600542_2050874606066190756_n.jpg?_nc_ht=instagram.fkhi10-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=hUzCn5rOvSUAX-xW682&oh=27066b0dbd54da3cff4683e60ae63f74&oe=5F798CA3" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+        <img src="https://instagram.fkhi10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.115.933.933a/s640x640/118408019_445411183083690_3655664231758381001_n.jpg?_nc_ht=instagram.fkhi10-1.fna.fbcdn.net&amp;_nc_cat=106&amp;_nc_ohc=W8HEpKz3x3cAX8BXZkk&amp;oh=e30b0d5f824a759327e5fd86d7ffb654&amp;oe=5F7734DE"class="w-100"  alt="">
+        </div>
+        <div class="col-4">
+        <img src="https://instagram.fkhi10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/118287795_1049613382163863_2826749017340034474_n.jpg?_nc_ht=instagram.fkhi10-1.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=_Gss_Crt864AX-4S3eS&amp;oh=db7cf4a29a62a46f367d0d73e31def20&amp;oe=5F7652A0"class="w-100"  alt="">
         </div>
     </div>
 </div>
