e8bc12d3897c89c9cd84e654a5de9e875ba389b1
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-09-01 17:07:53 +0500

    font changes in ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+        <div class="col-3 p-5">
+            <img src="https://instagram.fkhi10-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fkhi10-1.fna.fbcdn.net&_nc_ohc=2t8QuRbIh-0AX_CCvVP&oh=50e5df2c6b42b66be05dbb53c4192e21&oe=5F764467" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 p-5">
+                <div><h1>Free Code Camp</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>153</strong>Post</div>
+                    <div class="pr-5"><strong>23k</strong>Followers</div>
+                    <div class="pr-5"><strong>212</strong>Folowing</div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
